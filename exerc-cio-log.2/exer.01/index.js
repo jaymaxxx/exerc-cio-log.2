8dbd6212401a76012f77225575437abb999ddcdc
jaymaxxx <133407165+jaymaxxx@users.noreply.github.com>
2024-11-19 22:08:58 -0300

    exercicios logica

---
+++ exerc-cio-log.2/exer.01/index.js
@@ -0,0 +1,10 @@
+var num1 = parseInt(prompt("Digite o primeiro número: "))
+var num2 = parseInt(prompt("Digite o segundo número: "))
+if (num1 > num2) {
+    alert(num1)
+} else if (num2 > num1) {
+    alert(num2)
+} else {
+    alert("todos os numeros sao iguais")
+}
+
